97107040ffaa49c72144f40f049ec7c647bd5349
maqi <maqi@maqi.com>
2019-11-17 10:26:06 +0800

    工程化基础构建

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = function (env, argv) {
+  const isEnvDevelopment=argv.mode==='development'||!argv.mode;
+  const isEnvProduction=argv.mode==='production';
+  return {
+    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    devtool: isEnvProduction ? 'source-map' : isEnvDevelopment && 'cheap-module-source-map',
+    entry: {
+      app: './src/index.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: './public'
+    },
+    plugins: [
+      // new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "public/index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader'
+        }
+      ]
+    }
+  };
+}
