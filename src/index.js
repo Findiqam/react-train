bb90800987cb97ff1ea15340b3ba5d8a6b44dcfb
Findiqam <1141788876@qq.com>
2019-11-26 09:06:40 +0800

    测试机房提交

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 import 'font-awesome/css/font-awesome.min.css';
-
+//测试在机房电脑的git提交
 ReactDOM.render(
     <App />,
     document.getElementById('app')
